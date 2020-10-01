30914b5f86d5788d6f8b5753a27c5a2b89ff1cd6
Anna Shirokova <shirokova@avast.com>
2020-10-01 23:42:19 +0200

    add dynamic threshold via argparser, fixed formatting, fixed printing results to csv format

--- essh.py
+++ essh.py
@@ -6,16 +6,18 @@ from scapy.utils import rdpcap
 
 from settings import CONN_KEY, R_CONN_KEY
 
-__version__ = '?'
+__version__ = "?"
 
 
 def get_active_interface():
-    return netifaces.gateways()['default'][netifaces.AF_INET][1]
+    return netifaces.gateways()["default"][netifaces.AF_INET][1]
 
 
 def get_my_ip():
-    host_name = ([(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in
-                  [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
+    host_name = [
+        (s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close())
+        for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]
+    ][0][1]
     return str(host_name)
 
 
@@ -26,10 +28,10 @@ def get_ssh_sessions(pkt, sessions):
     dport = pkt[TCP].dport
 
     # Create a session
-    current_session_key = (ip_src, sport, ip_dst, dport, 'tcp')
+    current_session_key = (ip_src, sport, ip_dst, dport, "tcp")
     if_rcon_key = False
 
-    reverse_session_key = (ip_dst, dport, ip_src, sport, 'tcp')
+    reverse_session_key = (ip_dst, dport, ip_src, sport, "tcp")
     if reverse_session_key in sessions:
         if_rcon_key = True
         current_session_key = reverse_session_key
@@ -54,7 +56,10 @@ def is_established(sessions):
                 # Flags: PA=24, A=16, S=2, SA=18, FA=17, RA=20
                 if (packet_flags == 18) or (packet_flags == 24):
                     if tuple_key not in established_sessions:
-                        established_sessions[tuple_key] = {'ip1->ip2': [], 'ip2->ip1': []}
+                        established_sessions[tuple_key] = {
+                            "ip1->ip2": [],
+                            "ip2->ip1": [],
+                        }
                     established_sessions[tuple_key][direction].append(packet)
 
     return established_sessions
@@ -70,37 +75,81 @@ def get_seesion_size(sessions):
                 packet_size = packet.payload
                 session_size += len(packet_size)  # IP payload
                 if tuple_key not in session_packets_size:
-                    session_packets_size[tuple_key] = {'ip1->ip2': 0, 'ip2->ip1': 0}
+                    session_packets_size[tuple_key] = {"ip1->ip2": 0, "ip2->ip1": 0}
                 session_packets_size[tuple_key][direction] = session_size
     return session_packets_size
 
 
 def is_successful_login(size):
-    if size >= 2000:
-        return f"Successful SSH connection with session size {size}"
+    threshold = args.threshold
+    if size >= threshold:
+        return f"{size} Successful"
     else:
-        return f"Not successful SSH connection with session size {size}"
+        return f" {size} NotSuccessful"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Program 0.1 version {}. Author: xxx. ".format(__version__),
-                                     usage='%(prog)s -n <screen_name> [options]')
+    parser = argparse.ArgumentParser(
+        description="Program 0.1 version {}. Authors: Anna Shirokova, Sebastian Garcia ".format(
+            __version__
+        ),
+        usage="%(prog)s -n <ssh login detector> [options]",
+    )
 
-    parser.add_argument('-f', '--filename', help='Output file with the amount of packets per port in json format.',
-                        action='store',
-                        required=False)
-    parser.add_argument('-i', '--interface', help='Interface name to sniff the packets from', type=str, action='store',
-                        required=False)
-    parser.add_argument('-r', '--read', help='Enter pcap path to read', type=str, action='store',
-                        required=False)
-    parser.add_argument('-s', '--sniff', help='Put the IP and PORT to sniff on', type=str, action='store',
-                        required=False)
+    parser.add_argument(
+        "-f",
+        "--filename",
+        help="Output file with the amount of packets per port in json format.",
+        action="store",
+        required=False,
+    )
+    parser.add_argument(
+        "-i",
+        "--interface",
+        help="Interface name to sniff the packets from",
+        type=str,
+        action="store",
+        required=False,
+    )
+    parser.add_argument(
+        "-r",
+        "--read",
+        help="Enter pcap path to read",
+        type=str,
+        action="store",
+        required=False,
+    )
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        help="Choose threshold number. The default threshhold is 5340",
+        type=int,
+        default=5340,
+        required=False,
+    )
+    parser.add_argument(
+        "-s",
+        "--sniff",
+        help="Put the IP and PORT to sniff on",
+        type=str,
+        action="store",
+        required=False,
+    )
+
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        help="Choose threshold number",
+        type=int,
+        action="store",
+        required=False,
+    )
 
     args = parser.parse_args()
 
     if args.filename:
-        print(f'File: {args.filename}')
+        print(f"File: {args.filename}")
 
     if not args.interface:
         interface = get_active_interface()
@@ -113,15 +162,19 @@ if __name__ == '__main__':
     else:
         raise Exception("Pass pcap path as -r argument")
 
+    threshold = args.read
+
     sessions = {}
     for pkt in pkts:
-        if IP in pkt and TCP in pkt:
+        if IP in pkt and TCP in pkt and pkt[TCP].dport == 22:
             sessions = get_ssh_sessions(pkt, sessions)
 
     established_sessions = is_established(sessions)
 
     session_size = get_seesion_size(established_sessions)
 
-    for session_size in session_size.values():
+    for conn_tuple, session_size in session_size.items():
         total_session_size = sum(session_size.values())
-        print(f'Session {session_size} total session size {total_session_size} ')
+        is_successful = is_successful_login(total_session_size)
+        src_ip, src_p, dst_ip, dst_p, proto = conn_tuple
+        print(f"{src_ip},{src_p},{dst_ip},{dst_p},{proto},{is_successful}")
